73a95d66bfabbf6db0f6592f4099c922b61bc863
FrancescoFerretti8 <francescoferretti@gmail.com>
2025-06-21 15:53:13 +0000

    design finale2

--- mag/tcl/lvs_netgen.tcl
+++ mag/tcl/lvs_netgen.tcl
@@ -1,20 +1,15 @@
 set layout [readnet spice $project.lvs.spice]
 set source [readnet spice /dev/null]
-
 readnet spice $::env(PDK_ROOT)/$::env(PDK)/libs.ref/sky130_fd_sc_hd/spice/sky130_fd_sc_hd.spice $source
-
-# Celle analogiche personalizzate
-
-readnet spice /home/ttuser/Progetto_PISEI/xschem/simulation/Mux.spice $source
-
-# Top-level GL verilog
-#readnet verilog ../verilog/gl/Decoder.v $source
-
-#readnet verilog ../verilog/gl/$project.v $source
-
-# Modelli PDK SKY130
-#set ::env(LVS_EXTRA_LIBS) "$::env(PDK_ROOT)/sky130A/libs.tech/ngspice/sky130.lib.spice"
-
-# LVS con Decoder in blackbox (opzionale)
+ 
+# top level GL verilog
+readnet verilog ../src/project.v $source
+ 
+# add an GL verilog of any digital blocks:
+readnet verilog ../verilog/gl/Decoder.v $source
+ 
+# add any spice files of your analog blocks:
+readnet spice ../xschem/simulation/Mux.spice $source
+ 
 lvs "$layout $project" "$source $project" $::env(PDK_ROOT)/sky130A/libs.tech/netgen/sky130A_setup.tcl lvs.report -blackbox
 
